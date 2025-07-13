e4e6a6fda6fe98c9a3bffc4713a8dbcea7300a4b
kynex7510 <nikkitten3@gmail.com>
2025-07-13 14:13:21 +0200

    Add persp stereo tilt

--- Source/3ds.c
+++ Source/3ds.c
@@ -29,18 +29,9 @@ kmMat4* kmMat4OrthoTilt(kmMat4* pOut, kmScalar left, kmScalar right, kmScalar bo
 
     memset(pOut->mat, 0, sizeof(kmScalar) * 16);
 
-    /*
-    0                2/(top - bottom) 0              (bottom + top)/(bottom - top)
-    2/(left - right) 0                0              (left + right)/(right - left)
-
-
-    leftHanded:
-    0                0                1/(far - near) 0.5f*(near + far)/(near - far) - 0.5f
-    else:
-    0                0                1/(near - far) 0.5f*(near + far)/(near - far) - 0.5f
-
-    0                0                0              1
-    */
+    // Standard orthogonal projection matrix, with a fixed depth range of [-1,0] (required by PICA) and rotated τ/4 radians counterclockwise around the Z axis (due to 3DS screen orientation)
+	// http://www.wolframalpha.com/input/?i={{0,1,0,0},{-1,0,0,0},{0,0,1,0},{0,0,0,1}}{{1,0,0,0},{0,1,0,0},{0,0,0.5,-0.5},{0,0,0,1}}
+	// http://www.wolframalpha.com/input/?i={{0,1,0,0},{-1,0,0,0},{0,0,0.5,-0.5},{0,0,0,1}}{{2/(r-l),0,0,(l%2Br)/(l-r)},{0,2/(t-b),0,(b%2Bt)/(b-t)},{0,0,2/(n-f),(n%2Bf)/(n-f)},{0,0,0,1}}
 
     pOut->mat[1] = 2.0f/(left - right);
     pOut->mat[4] = 2.0f/(top - bottom);
@@ -57,4 +48,26 @@ kmMat4* kmMat4OrthoTilt(kmMat4* pOut, kmScalar left, kmScalar right, kmScalar bo
     pOut->mat[15] = 1.0f;
 
     return pOut;
+}
+
+kmMat4* kmMat4PerspStereoTilt(kmMat4* pOut, kmScalar fovx, kmScalar invaspect, kmScalar near, kmScalar far,
+    kmScalar iod, kmScalar screen, bool isLeftHanded) {
+    // Notes:
+	// Once again, we are passed "fovy" and the "aspect ratio"; however the 3DS screens are sideways,
+	// and the formula had to be tweaked. With stereo, left/right separation becomes top/bottom separation.
+	// The detailed mathematical explanation is in PerspTilt.
+
+    memset(pOut, 0, sizeof(kmMat4));
+
+	kmScalar fovx_tan = tanf(fovx/2.0f);
+	kmScalar fovx_tan_invaspect = fovx_tan*invaspect;
+	kmScalar shift = iod / (2.0f*screen); // 'near' not in the numerator because it cancels out in mat[9].
+
+    pOut->mat[1] = -1.0f / fovx_tan_invaspect;
+    pOut->mat[4] = 1.0f / fovx_tan;
+    pOut->mat[11] = isLeftHanded ? 1.0f : -1.0f;
+    pOut->mat[13] = iod / 2.0f;
+    pOut->mat[14] = near * far / (near - far);
+    pOut->mat[9] = -pOut->mat[11] * shift / fovx_tan_invaspect;
+    pOut->mat[10] = -pOut->mat[11] * near / (near - far);
 }
