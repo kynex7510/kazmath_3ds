bb800ac042b2b565bcb0c107e29020068aed5783
kynex7510 <nikkitten3@gmail.com>
2025-07-13 14:15:26 +0200

    Fix formatting

--- Source/3ds.c
+++ Source/3ds.c
@@ -30,8 +30,8 @@ kmMat4* kmMat4OrthoTilt(kmMat4* pOut, kmScalar left, kmScalar right, kmScalar bo
     memset(pOut->mat, 0, sizeof(kmScalar) * 16);
 
     // Standard orthogonal projection matrix, with a fixed depth range of [-1,0] (required by PICA) and rotated τ/4 radians counterclockwise around the Z axis (due to 3DS screen orientation)
-	// http://www.wolframalpha.com/input/?i={{0,1,0,0},{-1,0,0,0},{0,0,1,0},{0,0,0,1}}{{1,0,0,0},{0,1,0,0},{0,0,0.5,-0.5},{0,0,0,1}}
-	// http://www.wolframalpha.com/input/?i={{0,1,0,0},{-1,0,0,0},{0,0,0.5,-0.5},{0,0,0,1}}{{2/(r-l),0,0,(l%2Br)/(l-r)},{0,2/(t-b),0,(b%2Bt)/(b-t)},{0,0,2/(n-f),(n%2Bf)/(n-f)},{0,0,0,1}}
+    // http://www.wolframalpha.com/input/?i={{0,1,0,0},{-1,0,0,0},{0,0,1,0},{0,0,0,1}}{{1,0,0,0},{0,1,0,0},{0,0,0.5,-0.5},{0,0,0,1}}
+    // http://www.wolframalpha.com/input/?i={{0,1,0,0},{-1,0,0,0},{0,0,0.5,-0.5},{0,0,0,1}}{{2/(r-l),0,0,(l%2Br)/(l-r)},{0,2/(t-b),0,(b%2Bt)/(b-t)},{0,0,2/(n-f),(n%2Bf)/(n-f)},{0,0,0,1}}
 
     pOut->mat[1] = 2.0f/(left - right);
     pOut->mat[4] = 2.0f/(top - bottom);
@@ -53,15 +53,15 @@ kmMat4* kmMat4OrthoTilt(kmMat4* pOut, kmScalar left, kmScalar right, kmScalar bo
 kmMat4* kmMat4PerspStereoTilt(kmMat4* pOut, kmScalar fovx, kmScalar invaspect, kmScalar near, kmScalar far,
     kmScalar iod, kmScalar screen, bool isLeftHanded) {
     // Notes:
-	// Once again, we are passed "fovy" and the "aspect ratio"; however the 3DS screens are sideways,
-	// and the formula had to be tweaked. With stereo, left/right separation becomes top/bottom separation.
-	// The detailed mathematical explanation is in PerspTilt.
+    // Once again, we are passed "fovy" and the "aspect ratio"; however the 3DS screens are sideways,
+    // and the formula had to be tweaked. With stereo, left/right separation becomes top/bottom separation.
+    // The detailed mathematical explanation is in PerspTilt.
 
     memset(pOut, 0, sizeof(kmMat4));
 
-	kmScalar fovx_tan = tanf(fovx/2.0f);
-	kmScalar fovx_tan_invaspect = fovx_tan*invaspect;
-	kmScalar shift = iod / (2.0f*screen); // 'near' not in the numerator because it cancels out in mat[9].
+    kmScalar fovx_tan = tanf(fovx/2.0f);
+    kmScalar fovx_tan_invaspect = fovx_tan*invaspect;
+    kmScalar shift = iod / (2.0f*screen); // 'near' not in the numerator because it cancels out in mat[9].
 
     pOut->mat[1] = -1.0f / fovx_tan_invaspect;
     pOut->mat[4] = 1.0f / fovx_tan;
